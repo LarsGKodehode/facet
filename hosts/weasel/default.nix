532c7e2bee1d358ea6f95d24132e3299dc4a93a5
zabronax <zabronax@gmail.com>
2024-07-20 23:29:03 +0000

    set weasel wsl configurations

--- hosts/weasel/default.nix
+++ hosts/weasel/default.nix
@@ -1,19 +1,40 @@
 # The Weasel
-# System configuration for a WSL instance
+# System configuration for the basic WSL instance
 
 {
   inputs,
   globals,
+  # Disabling unlisted dependencies to better understand when it breaks 
+  #...
 }:
 
+let
+  hostName = "weasel";
+in
 inputs.nixpkgs.lib.nixosSystem {
   system = "x86_64-linux";
   modules = [
-    inputs.nixos-wsl.nixosModules.default
+    inputs.nixos-wsl.nixosModules.wsl
     {
+      # The initial version of NixOS installed
+      # used in as part of system migrations 
+      # TODO figure out where to best define this
       system.stateVersion = "24.05";
-      wsl.enable = true;
-      wsl.defaultUser = globals.user;
+
+      networking.hostName = hostName; 
+
+      wsl = {
+        # Documentation
+        # https://nix-community.github.io/NixOS-WSL/
+        enable = true;
+
+	defaultUser = globals.user;
+
+        wslConf.network.generateResolvConf = true; # Turn off if it breaks VPN
+	interop.includePath = false; # Slows down some shell operations due to filesystem boundary
+      };
+
+      nix.settings.experimental-features = "nix-command flakes";
     }
   ];
 }
