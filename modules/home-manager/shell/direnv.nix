a3367afd86831de525ca8386ad70ad21167fcc4b
zabronax <zabronax@gmail.com>
2024-07-23 22:31:37 +0000

    set direnv to be silent

--- modules/home-manager/shell/direnv.nix
+++ modules/home-manager/shell/direnv.nix
@@ -8,9 +8,17 @@
 }:
 
 {
-  home-manager.users.${config.user}.programs.direnv = {
-    enable = true;
-    nix-direnv.enable = true;
-    config.whitelist.prefix = [ config.nixConfigurationsPath ];
+  home-manager.users.${config.user} = {
+    programs.direnv = {
+      enable = true;
+      nix-direnv.enable = true;
+
+      # Always trust the nixos configuration
+      config.whitelist.prefix = [ config.nixConfigurationsPath ];
+
+      # Silence the environment variable dump due to verbosity
+      # when loading or rebuilding nix-shells
+      silent = true;
+    };
   };
 }
