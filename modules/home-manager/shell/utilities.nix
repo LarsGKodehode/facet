ac98c56ddd90f9abebfe7531ffa00a120f1b643a
zabronax <zabronax@gmail.com>
2024-07-27 12:24:14 +0000

    add abbreviation for directory listing

--- modules/home-manager/shell/utilities.nix
+++ modules/home-manager/shell/utilities.nix
@@ -7,14 +7,20 @@
 }:
 
 {
-  home-manager.users.${config.user}.home.packages = [
-    pkgs.tree # Directory hierarchy visualizer
-    pkgs.jq # JSON processor
-    pkgs.htmlq # HTML processor
-    pkgs.gawk # awk String processor
-    pkgs.ripgrep # grep replacement
-    pkgs.sd # Sed
+  home-manager.users.${config.user} = {
+    home.packages = [
+      pkgs.tree # Directory hierarchy visualizer
+      pkgs.jq # JSON processor
+      pkgs.htmlq # HTML processor
+      pkgs.gawk # awk String processor
+      pkgs.ripgrep # grep replacement
+      pkgs.sd # Sed
 
-    pkgs.nil # Nix language server
-  ];
+      pkgs.nil # Nix language server
+    ];
+
+    programs.fish.shellAbbrs = {
+      t = "tree --dirsfirst -C";
+    };
+  };
 }
